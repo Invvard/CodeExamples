6a6b7ba4028782ebc43796ac5160e03dcc448c55
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-14 00:33:46 -0400

    Add PluginLoader interface and implementation

---
+++ InvvardDev.Examples.PluginApp/InvvardDev.Examples.PluginApp.MainApp/Tools/IPluginLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace InvvardDev.Examples.PluginApp.MainApp.Tools
+{
+    public interface IPluginLoader<T>
+    {
+        IEnumerable<T> Plugins { get; set; }
+
+        void Dispose();
+    }
+}
