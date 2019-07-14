5bfa643c1afd995e105a1ffdba4f791f30c42ab2
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-13 22:26:00 -0400

    Add the IKeyboardModel interface

---
+++ InvvardDev.Examples.PluginApp/InvvardDev.Examples.PluginApp.Models/IKeyboardModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace InvvardDev.Examples.PluginApp.Models
+{
+    public interface IKeyboardModel
+    {
+        /// <summary>
+        /// Gets or sets the keyboard model name.
+        /// </summary>
+        string ModelName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the keyboard list of keys.
+        /// </summary>
+        List<string> Keys { get; set; }
+
+        /// <summary>
+        /// Loads the layout from data.
+        /// </summary>
+        /// <param name="data">Data from settings repository</param>
+        /// <returns><c>True</c> if loading is successful.</returns>
+        bool LoadLayout(string data);
+    }
+}
