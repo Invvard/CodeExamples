0a7c9c168ccd22f52bbc0dc2118fdb0e5a638433
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-14 00:32:44 -0400

    Change Plugin Project to Framework  4.8

---
+++ InvvardDev.Examples.PluginApp/InvvardDev.Plugin.ErgoDoxEz/ErgoDoxEz.cs
@@ -0,0 +1,27 @@
+﻿using InvvardDev.Examples.PluginApp.Models;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace InvvardDev.Plugin.ErgoDoxEz
+{
+    [Export(typeof(IKeyboardModel))]
+    public class ErgoDoxEz : IKeyboardModel
+    {
+        public string ModelName { get; private set; }
+        public List<string> Keys { get; private set; }
+
+        public ErgoDoxEz()
+        {
+            ModelName = "ErgoDox EZ";
+        }
+
+        public bool LoadLayout()
+        {
+            Keys = new List<string> {
+                                        "A", "B", "C", "D", "E", "F", "G"
+                                    };
+
+            return true;
+        }
+    }
+}
