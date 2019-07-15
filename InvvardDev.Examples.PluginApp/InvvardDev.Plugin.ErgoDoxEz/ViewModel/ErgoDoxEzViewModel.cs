24c85ab983db71d006297bca6e9879aeb49d385b
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-14 22:06:52 -0400

    Add the plugin view model

---
+++ InvvardDev.Examples.PluginApp/InvvardDev.Plugin.ErgoDoxEz/ViewModel/ErgoDoxEzViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace InvvardDev.Plugin.ErgoDoxEz.ViewModel
+{
+    public class ErgoDoxEzViewModel : ViewModelBase
+    {
+        private string _firstRadioButtonContent;
+        private string _secondRadioButtonContent;
+
+        public string FirstRadioButtonContent
+        {
+            get => _firstRadioButtonContent;
+            set => Set(ref _firstRadioButtonContent, value);
+        }
+
+        public string SecondRadioButtonContent
+        {
+            get => _secondRadioButtonContent;
+            set => Set(ref _secondRadioButtonContent, value);
+        }
+
+        public ErgoDoxEzViewModel()
+        {
+            FirstRadioButtonContent = "Radio button 1";
+            SecondRadioButtonContent = "Radio button 2";
+        }
+    }
+}
