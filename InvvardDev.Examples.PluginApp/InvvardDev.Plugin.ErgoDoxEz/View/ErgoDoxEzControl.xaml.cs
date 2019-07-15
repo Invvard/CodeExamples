1eca879a96eb95723f8f007e74f34a0d5e920726
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-14 22:06:37 -0400

    Add the plugin view

---
+++ InvvardDev.Examples.PluginApp/InvvardDev.Plugin.ErgoDoxEz/View/ErgoDoxEzControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace InvvardDev.Plugin.ErgoDoxEz.View
+{
+    /// <summary>
+    /// Interaction logic for ErgoDoxEzView.xaml
+    /// </summary>
+    public partial class ErgoDoxEzControl
+    {
+        public ErgoDoxEzControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
