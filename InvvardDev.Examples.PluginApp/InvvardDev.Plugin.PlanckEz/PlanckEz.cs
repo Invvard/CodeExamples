6dd7e78e8a0ae0a78dd980bdd6a215ad0c3da6de
Pierre CAVAROC <pierre.cavaroc@exfo.com>
2019-07-15 13:11:14 -0400

    Create the second plugin

---
+++ InvvardDev.Examples.PluginApp/InvvardDev.Plugin.PlanckEz/PlanckEz.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using InvvardDev.Examples.PluginApp.Models;
+
+namespace InvvardDev.Plugin.PlanckEz
+{
+    [Export(typeof(IKeyboardModel))]
+    public class PlanckEz : IKeyboardModel
+    {
+        public string ModelName { get; }
+        public List<string> Keys { get; }
+
+        public PlanckEz()
+        {
+            ModelName = "Planck EZ";
+        }
+
+        public bool LoadLayout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object GetKeyboardView()
+        {
+            return new List<string>();
+        }
+    }
+}
