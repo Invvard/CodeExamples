698be407ed57e163907da321cc091bfd0d4cf0a5
Pierre CAVAROC <pierre.cavaroc@exfo.com>
2019-08-08 14:24:41 -0400

    Get the file list to retrieve

--- python/GetSourceMap.py
+++ python/GetSourceMap.py
@@ -1,8 +1,31 @@
 import urllib.request
+import re
 
-url = 'https://configure.ergodox-ez.com/layouts/EOEb/latest/0'
-response = urllib.request.urlopen(url)
-data = response.read()
-text = data.decode('utf-8')
+sc = 'https://configure.ergodox-ez.com/layouts/EOEb/latest/0'
 
-print(text)
+
+def get_pagecontent(url):
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    page_content = data.decode('utf-8')
+    return page_content
+
+
+def get_filenames(text):
+    file_list = []
+    pattern_source_maps = r'"static/js/"\+\(\{\}\[l\]\|\|l\)\+"\."\+\{(?P<sourcemaps>(?:\d:"[a-f0-9]+",?)*?)}'
+    pattern_files = r'(?P<index>\d):"(?P<filename>[a-f0-9]+)"'
+
+    source_maps = re.search(pattern_source_maps, text)
+    if source_maps:
+        files = re.finditer(pattern_files, source_maps.group('sourcemaps'))
+        for file in files:
+            file_list.append(file.group('filename'))
+
+    return file_list
+
+
+content = get_pagecontent('https://configure.ergodox-ez.com/layouts/EOEb/latest/0')
+fileList = get_filenames(content)
+
+print(fileList)
